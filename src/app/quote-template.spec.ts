db5387ca05d9787419a861bab6250b50fe344403
Ondigo Wendy <wendy.ondigo@student.moringaschool.com>
2021-08-21 01:34:31 +0300

    Create a template for my quotes by generating a class and adding a constructor then binding to the app html file.

---
+++ src/app/quote-template.spec.ts
@@ -0,0 +1,7 @@
+import { QuoteTemplate } from './quote-template';
+
+describe('QuoteTemplate', () => {
+  it('should create an instance', () => {
+    expect(new QuoteTemplate()).toBeTruthy();
+  });
+});
---
+++ src/app/quote-template.ts
@@ -0,0 +1,5 @@
+export class QuoteTemplate {
+  constructor( public id: number ,public quote: string,public author: string,public uploader: string, public completeDate: Date)
+  {  
+}
+}
