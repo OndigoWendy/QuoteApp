e879957da9169bc30c97effe0742fb092100c887
Ondigo Wendy <wendy.ondigo@student.moringaschool.com>
2021-08-23 01:45:28 +0300

    Add styles to the quote cards for better visualisation and easier scanning.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+
 import { Component } from '@angular/core';
 import { Quote } from './quote';
 @Component({
@@ -7,14 +8,6 @@ import { Quote } from './quote';
 })
 export class AppComponent {
   title = 'quotes-app';
-  quotes: Quote[] = [
-    new Quote(1,'The future belongs to those who believe in the beauty of their dreams.', 'Pinky Jude', 'uploader',new Date(2020,3,14),0,0),
-     new Quote(2,'The best is yet to come', 'Wendy Ondigo', 'uploader',new Date(2019,6,9),0,0),
-    // new QuoteTemplate(3,'my quote', 'Author', 'uploader',new Date(2022,1,12)),
-    // new QuoteTemplate(4,'my quote', 'Author', 'uploader',new Date(2019,0,18)),
-    // new QuoteTemplate(5,'my quote', 'Author', 'uploader',new Date(2019,2,14)),
-    // new QuoteTemplate(6,'my quote', 'Author', 'uploader',new Date(2030,3,14)),
-  ];
 
   addNewQuote(quote: Quote){
     let quoteLength = this.quotes.length;
@@ -23,12 +16,6 @@ export class AppComponent {
     this.quotes.push(quote)
   }
 
-  completeQuote(isComplete: any, index: number){
-    if (isComplete) {
-      this.quotes.splice(index,1);
-    }
-  }
-  
   deleteQuote(isComplete: any, index: number){
     if (isComplete) {
       let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].quote}?`)
@@ -38,7 +25,26 @@ export class AppComponent {
       }
     }
   }
-  constructor() { }
+  
+  quotes: Quote[] = [
+    new Quote(1,'The future belongs to those who believe in the beauty of their dreams.', 'Pinky Jude', 'uploader',new Date(2020,3,14),0,0),
+     new Quote(2,'The best is yet to come', 'Wendy Ondigo', 'Wendy Ondigo',new Date(2019,6,9),0,0),
+     new Quote(3,'The sun will rise and we will try again.', 'Sarafina M.','Anita Baker' ,new Date(2022,1,12),0,0),
+    new Quote(4,'Never give up','Fifi A.','Sally N.',new Date(2019,0,18),0,0),
+   
+  ];
+
+  
+
+  // completeQuote(isComplete: any, index: number){
+  //   if (isComplete) {
+  //     this.quotes.splice(index,1);
+  //   }
+  // }
+  
+  
+  constructor() {
+  }
 
   ngOnInit() {
   }
