ebb1b009404713cc82d5fcfd81b18529dbb08451
Ondigo Wendy <wendy.ondigo@student.moringaschool.com>
2021-08-22 21:51:31 +0300

    Create a new component to include details about the the quote eg delete option.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { QuoteInfoComponent } from './quote-info/quote-info.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteInfoComponent,
+    QuoteFormComponent,
+
+   
+    
+   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
