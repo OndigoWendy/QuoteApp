8ca8a66048ff9c130b4342d8eaccceebbde4d3b9
Ondigo Wendy <wendy.ondigo@student.moringaschool.com>
2021-08-23 02:32:30 +0300

    Fix the add quote and delete quote button by changing the logic in the component.ts file.

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output,EventEmitter } from '@angular/core';
 import { Quote } from '../quote';
+import { FormGroup,NgForm } from '@angular/forms';
 @Component({
   selector: 'app-quote-form',
   templateUrl: './quote-form.component.html',
@@ -9,10 +10,19 @@ export class QuoteFormComponent implements OnInit {
 newGoal = new Quote(0,"","","",new Date(),0,0);
 
 @Output() addQuote = new EventEmitter<Quote>();
-  newQuote: any;
+   newQuote: any;
 
-submitQuote(){
-this.addQuote.emit(this.newQuote);
+// submitQuote(){
+// this.addQuote.emit(this.newQuote);
+// }
+submitQuote(f:NgForm){
+  if(f.valid){
+    this.addQuote.emit(this.newQuote);
+    this.newQuote = new Quote(0,"","","", new Date(),0,0);
+  }else{
+    alert("Please enter valid inputs")
+  }
+
 }
   constructor() { }
 
