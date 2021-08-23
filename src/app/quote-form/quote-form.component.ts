8a0a49667ccef80e50ccf49d0fc86382733b8ea3
Ondigo Wendy <wendy.ondigo@student.moringaschool.com>
2021-08-23 18:20:35 +0300

    Adjust the add quote logic for better output.

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -7,23 +7,25 @@ import { FormGroup,NgForm } from '@angular/forms';
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
-newGoal = new Quote(0,"","","",new Date(),0,0);
+newQuote = new Quote(0,"","","",new Date(),0,0);
 
 @Output() addQuote = new EventEmitter<Quote>();
-   newQuote: any;
+  
 
 // submitQuote(){
 // this.addQuote.emit(this.newQuote);
 // }
-submitQuote(f:NgForm){
-  if(f.valid){
-    this.addQuote.emit(this.newQuote);
-    this.newQuote = new Quote(0,"","","", new Date(),0,0);
-  }else{
-    alert("Please enter valid inputs")
+
+submitQuote(){
+  // if(f.valid){
+  //   this.addQuote.emit(this.newQuote);
+  //   this.newQuote = new Quote(0,"","","", new Date(),0,0);
+  // }else{
+  //   alert("Invalid input")
+  this.addQuote.emit(this.newQuote);
   }
 
-}
+
   constructor() { }
 
   ngOnInit(): void {
