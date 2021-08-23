b2fba1573e2a8c40a1eb216b650ac86861dbd8f5
Ondigo Wendy <wendy.ondigo@student.moringaschool.com>
2021-08-23 14:40:59 +0300

    Minor style changes in the app component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,8 +29,8 @@ export class AppComponent {
   quotes: Quote[] = [
     new Quote(1,'The future belongs to those who believe in the beauty of their dreams.', 'Pinky Jude', 'uploader',new Date(2020,3,14),0,0),
      new Quote(2,'The best is yet to come', 'Wendy Ondigo', 'Wendy Ondigo',new Date(2019,6,9),0,0),
-     new Quote(3,'The sun will rise and we will try again.', 'Sarafina M.','Anita Baker' ,new Date(2022,1,12),0,0),
-    new Quote(4,'Never give up','Fifi A.','Sally N.',new Date(2019,0,18),0,0),
+    //  new Quote(3,'The sun will rise and we will try again.', 'Sarafina M.','Anita Baker' ,new Date(2022,1,12),0,0),
+    // new Quote(4,'Never give up','Fifi A.','Sally N.',new Date(2019,0,18),0,0),
    
   ];
 
