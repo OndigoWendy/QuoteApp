90ac7693a15a6a6da069a1cc0e2f4bd1bc5b4fd5
Ondigo Wendy <wendy.ondigo@student.moringaschool.com>
2021-08-23 16:49:18 +0300

    Adjust the pipe logic to calculate time lapse.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,8 +27,8 @@ export class AppComponent {
   }
   
   quotes: Quote[] = [
-    new Quote(1,'The future belongs to those who believe in the beauty of their dreams.', 'Pinky Jude', 'uploader',new Date(2020,3,14),0,0),
-     new Quote(2,'The best is yet to come', 'Wendy Ondigo', 'Wendy Ondigo',new Date(2019,6,9),0,0),
+    new Quote(1,'The future belongs to those who believe in the beauty of their dreams.', 'Pinky Jude', 'Andre Wan',new Date(2020,3,14),0,0),
+     new Quote(2,'The best is yet to come', 'Polar Rosan', 'Wendy Ondigo',new Date(2019,6,9),0,0),
     //  new Quote(3,'The sun will rise and we will try again.', 'Sarafina M.','Anita Baker' ,new Date(2022,1,12),0,0),
     // new Quote(4,'Never give up','Fifi A.','Sally N.',new Date(2019,0,18),0,0),
    
@@ -36,11 +36,11 @@ export class AppComponent {
 
   
 
-  // completeQuote(isComplete: any, index: number){
-  //   if (isComplete) {
-  //     this.quotes.splice(index,1);
-  //   }
-  // }
+  completeQuote(isComplete: any, index: number){
+    if (isComplete) {
+      this.quotes.splice(index,1);
+    }
+  }
   
   
   constructor() {
