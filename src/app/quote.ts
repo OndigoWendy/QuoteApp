2be0cc8154246f8cd623b874c4e1ab2f1c8dd764
Ondigo Wendy <wendy.ondigo@student.moringaschool.com>
2021-08-23 04:14:14 +0300

    Auto-generated commit [ci skip]

--- src/app/quote.ts
+++
@@ -1,5 +0,0 @@
-export class Quote {
-  constructor( public id: number ,public quote: string,public author: string,public uploader: string, public completeDate: Date,public likes:number,public dislikes:number)
-  {  
-}
-}
